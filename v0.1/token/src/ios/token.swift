5f3b076a8ed9ea0aec59ab67d6a0f3c93d708dc6
Almoayed Technologies Admin <46859386+almoayedtechnologies@users.noreply.github.com>
2019-06-10 14:10:55 +0300

    Adding subscribe API
    call subscribe method with the memberId received during createMember call.

--- v0.1/token/src/ios/token.swift
+++ v0.1/token/src/ios/token.swift
@@ -1,57 +1,105 @@
 import TokenSdk
 
-class token : CDVPlugin {
-  @objc(createMember:)
+class token: CDVPlugin {
+    let developerKey: NSString = "4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI"
+    let env: TokenCluster = TokenCluster.sandbox()
+    let realm = "at-bisb"
     
+    // create and return a token client
     
-  func createMember(mobileNumber: CDVInvokedUrlCommand) {
-
-        print("received mobileNumber",mobileNumber)
-   
-        let taskQ = DispatchQueue(label: "perform_task")
+    func getTokenClient() -> TokenClient {
+        let builder: TokenClientBuilder?
+        builder = TokenClientBuilder()
+        builder?.tokenCluster = TokenCluster.sandbox()
+        builder?.port = 443
+        builder?.useSsl = true
+        builder?.developerKey = "4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI"
+        return (builder?.build())!
+    }
     
-        taskQ.sync {
-
-            print("create a token client object")
-
-            var builder :TokenClientBuilder?
-            builder = TokenClientBuilder()
-            builder?.tokenCluster = TokenCluster.sandbox()
-            builder?.port = 443
-            builder?.useSsl = true
-            builder?.developerKey = "4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI"
-
-            var tokenClient :TokenClient?
-            tokenClient = builder?.build()
-            print("token client",tokenClient)
-
-            var alias  = Alias()
-            alias.type = Alias_Type.phone
-            alias.realm = "at-bisb"
-            alias.value = "+97333263343"
-
-            print("passed alias",alias)
-
-            tokenClient?.createMember(alias, recoveryAgent: "at-bisb", onSuccess: { (TKMember) in
-                print("created a member",TKMember)
-            }, onError: { (Error) in
-                print("failed to created a member",Error)
-            })
+    // create and return an alias object
+    
+    func makeAliasObject(value: String) -> Alias {
+        let alias = Alias()
+        alias.type = Alias_Type.phone
+        alias.realm = realm
+        alias.value = value
+        return alias
+    }
+    
+    // create a member and return memberId
+    
+    @objc(createMember:)
+    func createMember(mobileNumber: CDVInvokedUrlCommand) {
+        var memberId = ""
+        
+        let mobileNumberStr = mobileNumber.arguments[0] as? String ?? ""
+        
+        print("creating member with alias", mobileNumberStr)
+        
+        let dispatchGrp = DispatchGroup()
+        dispatchGrp.enter()
+        
+        getTokenClient().createMember(makeAliasObject(value: "+97333209987"), recoveryAgent: realm, onSuccess: { TKMember in
+            print("createMember:success", TKMember.id)
+            memberId = TKMember.id
             
+            dispatchGrp.leave()
+        }, onError: { Error in
+            print("createMember:failed", Error)
+            dispatchGrp.leave()
+        })
+        
+        dispatchGrp.notify(queue: .main) {
+            print("main notified with member: ", memberId)
+            var pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK,
+                messageAs: memberId
+            )
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: mobileNumber.callbackId
+            )
         }
-
-    var pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_ERROR
-    )
+    }
     
-    
-    self.commandDelegate!.send(
-        pluginResult,
-        callbackId: mobileNumber.callbackId
-    )
- 
-  }
-    
-    
-    
+    @objc(subscribe:)
+    func subscribe(memberId: CDVInvokedUrlCommand) {
+        var subscriberId = ""
+        let memberIdStr = memberId.arguments[0] as? String ?? ""
+        
+        print("subscribing for member ", memberIdStr)
+        
+        let dispatchGrp = DispatchGroup()
+        dispatchGrp.enter()
+        
+        getTokenClient().getMember(memberIdStr, onSuccess: { Member in
+            Member.subscribe(toNotifications: self.realm, handlerInstructions: nil, onSuccess: { Subscriber in
+                subscriberId = Subscriber.id_p
+                print("subscriber id is ", Subscriber.id_p)
+                dispatchGrp.leave()
+                
+            }, onError: { Error in
+                print("subcription error ", Error)
+                dispatchGrp.leave()
+                
+            })
+        }, onError: { Error in
+            print("error during subcription ", Error)
+            dispatchGrp.leave()
+            
+        })
+        
+        dispatchGrp.notify(queue: .main) {
+            print("main notified with member: ", subscriberId)
+            var pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK,
+                messageAs: subscriberId
+            )
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: memberId.callbackId
+            )
+        }
+    }
 }
