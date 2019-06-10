99b5b9270c8f4180db37eb18b224e58820cc7e20
Almoayed Technologies Admin <46859386+almoayedtechnologies@users.noreply.github.com>
2019-06-10 14:12:03 +0300

    Subscribe interface added

--- v0.1/token/www/token.js
+++ v0.1/token/www/token.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.createMember = function (arg0, success, error) {
     exec(success, error, 'token', 'createMember', [arg0]);
 };
+
+exports.subscribe = function (arg0, success, error) {
+    exec(success, error, 'token', 'subscribe', [arg0]);
+};
