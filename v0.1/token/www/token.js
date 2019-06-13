d77115eae346936a43f0af36286214fc5118d958
Ashok <ashok@almoayedtechnologies.com>
2019-06-13 14:10:11 +0300

    updated plugin with linking

--- v0.1/token/www/token.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.createMember = function (arg0, success, error) {
-    exec(success, error, 'token', 'createMember', [arg0]);
-};
-
-exports.subscribe = function (arg0, success, error) {
-    exec(success, error, 'token', 'subscribe', [arg0]);
-};
