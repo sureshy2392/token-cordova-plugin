ab2d4be2b8f8e22f1d262a2f1cad89c3eeadea0f
Almoayed Technologies Admin <46859386+almoayedtechnologies@users.noreply.github.com>
2019-06-15 06:57:02 +0300

    new methods

--- token/www/token.js
+++ token/www/token.js
@@ -15,3 +15,31 @@ exports.linkAccounts = function (arg0, success, error) {
 exports.getAccounts = function (arg0, success, error) {
     exec(success, error, 'token', 'getAccounts', [arg0]);
 };
+
+exports.getAccount = function (arg0, success, error) {
+    exec(success, error, 'token', 'getAccount', [arg0]);
+};
+
+exports.getConsents = function (arg0, success, error) {
+    exec(success, error, 'token', 'getConsents', [arg0]);
+};
+
+exports.getTransfers = function (arg0, success, error) {
+    exec(success, error, 'token', 'getTransfers', [arg0]);
+};
+
+exports.getProfile = function (arg0, success, error) {
+    exec(success, error, 'token', 'getProfile', [arg0]);
+};
+
+exports.getProfilePicture = function (arg0, success, error) {
+    exec(success, error, 'token', 'getProfilePicture', [arg0]);
+};
+
+exports.unlinkAccounts = function (arg0, success, error) {
+    exec(success, error, 'token', 'unlinkAccounts', [arg0]);
+};
+
+exports.deleteMember = function (arg0, success, error) {
+    exec(success, error, 'token', 'deleteMember', [arg0]);
+};
